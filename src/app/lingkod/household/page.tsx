5677a07961b68364a62f585d96919f2e62a0e3e5
Jonathan Moralde <jonathanhernandez.moralde@bicol-u.edu.ph>
2024-09-05 23:38:44 +0800

    test force dynamic on household

--- src/app/lingkod/household/page.tsx
+++ src/app/lingkod/household/page.tsx
@@ -2,20 +2,19 @@ import React from "react";
 import { columns, HouseholdDetail } from "./columns";
 import { DataTable } from "@/components/lingkod/data-table";
 import { getData } from "./actions";
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
 
-export const revalidate = 60;
+// export async function getServerSideProps() {
+//   // Fetch data from external API
+//   const data: HouseholdDetail[] = await getData();
 
-// We'll prerender only the params from `generateStaticParams` at build time.
-// If a request comes in for a path that hasn't been generated,
-// Next.js will server-render the page on-demand.
-export const dynamicParams = true; // or false, to 404 on unknown paths
-
-export async function generateStaticParams() {
-  const data: HouseholdDetail[] = await getData();
-  // This is only needed if you have dynamic routes like [id].tsx
-  // return data.map(item => ({ id: item.id })); // Assuming you have dynamic paths
-  return []; // Otherwise, return an empty array if no dynamic params
-}
+//   // Pass data to the page via props
+//   return { props: { data } };
+// }
+// const Household = async ({
+//   data,
+// }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+export const dynamic = "force-dynamic";
 const Household = async () => {
   const data: HouseholdDetail[] = await getData();
 
