ff2cef330583707d8029346f9e43a0f4ba4b5ad4
Jonathan Moralde <jonathanhernandez.moralde@bicol-u.edu.ph>
2024-09-05 22:57:08 +0800

    converted household page from SSG to ISR

--- src/app/lingkod/household/page.tsx
+++ src/app/lingkod/household/page.tsx
@@ -3,10 +3,22 @@ import { columns, HouseholdDetail } from "./columns";
 import { DataTable } from "@/components/lingkod/data-table";
 import { getData } from "./actions";
 
-type Props = {};
+export const revalidate = 60;
 
-const Household = async (props: Props) => {
+// We'll prerender only the params from `generateStaticParams` at build time.
+// If a request comes in for a path that hasn't been generated,
+// Next.js will server-render the page on-demand.
+export const dynamicParams = true; // or false, to 404 on unknown paths
+
+export async function generateStaticParams() {
   const data: HouseholdDetail[] = await getData();
+  // This is only needed if you have dynamic routes like [id].tsx
+  // return data.map(item => ({ id: item.id })); // Assuming you have dynamic paths
+  return []; // Otherwise, return an empty array if no dynamic params
+}
+const Household = async () => {
+  const data: HouseholdDetail[] = await getData();
+
   return (
     <section className="bg-indigo-950 rounded-xl px-4 py-10  h-[80vh]">
       <h3 className="text-xl font-semibold mb-2">Household</h3>
--- src/app/lingkod/request/page.tsx
+++ src/app/lingkod/request/page.tsx
@@ -3,6 +3,7 @@ import { DataTable } from "@/components/lingkod/data-table";
 import { Timestamp } from "firebase/firestore";
 import React from "react";
 import { getData } from "./actions";
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
 
 const Request = async () => {
   const requests: DocRequest[] = await getData();
