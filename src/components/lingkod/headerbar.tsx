a996729aa20eff1523a88fe3e7b132b89528ffe8
Jonathan Moralde <jonathanhernandez.moralde@bicol-u.edu.ph>
2024-09-06 23:55:11 +0800

    functional notification

--- src/components/lingkod/headerbar.tsx
+++ src/components/lingkod/headerbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LogOut, Settings, UserPlus } from "lucide-react";
+import { Loader2, LogOut, Settings, UserPlus } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -17,17 +17,49 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+import { Badge } from "@/components/ui/badge";
+
 import React, { useEffect, useState } from "react";
-import { FaCalendarAlt, FaUserCircle } from "react-icons/fa";
+import { FaCalendarAlt, FaUserCircle, FaBell } from "react-icons/fa";
 
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import { deleteAuthCookie } from "@/app/action";
 import { useAuth } from "@/app/context/AuthContext";
 import Link from "next/link";
+import { ScrollArea } from "../ui/scroll-area";
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  Timestamp,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { db } from "@/config/firebase";
+import { differenceInDays, format, formatDistanceToNow } from "date-fns";
+import NotificationCard from "./notification-card";
+
+export type NotificationData = {
+  id: string;
+  notif_msg: string;
+  receiver_uid: string;
+  type: string;
+  timestamp: string;
+};
 
 const HeaderBar = () => {
   const [currentDate, setCurrentDate] = useState<Date>(new Date());
+  const [loading, setLoading] = useState<boolean>(false);
+  const [notifications, setNotifications] = useState<NotificationData[]>([]);
+  const [popoverOpen, setPopoverOpen] = useState<boolean>(false);
 
   const router = useRouter();
   const { user, logout, userDetails } = useAuth();
@@ -76,6 +108,128 @@ const HeaderBar = () => {
     }
   };
 
+  const handleNotif = async () => {
+    setLoading(true);
+    console.log(user?.uid);
+    try {
+      const notifRef = query(
+        collection(db, "notifications"),
+        where("receiver_uid", "==", user?.uid),
+        where("is_read", "==", false)
+      );
+      const notifSnapshot = await getDocs(notifRef);
+
+      if (notifSnapshot.docs.length >= 1) {
+        const data: NotificationData[] = notifSnapshot.docs.map((doc) => {
+          const docData = doc.data();
+
+          const timestamp = (docData.timestamp as Timestamp).toDate();
+          const daysDifference = differenceInDays(new Date(), timestamp);
+
+          // If the difference is 7 days or less, show "time ago"
+          const timeAgo = formatDistanceToNow(timestamp, { addSuffix: true });
+
+          // If older than 7 days, show formatted date
+          const formattedDate = format(timestamp, "MMMM dd, yyyy");
+
+          // Decide which one to display
+          const displayDate = daysDifference <= 7 ? timeAgo : formattedDate;
+
+          return {
+            id: doc.id,
+            notif_msg: docData.notif_msg,
+            receiver_uid: docData.receiver_uid,
+            type: docData.type,
+            timestamp: displayDate,
+          };
+        });
+
+        setNotifications(data);
+      } else {
+        setNotifications([]); // Clear notifications if none are found
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("Error message:", error.message);
+
+        toast.error(`Failed to fetch notifications. ${error.message}`);
+      } else {
+        console.error("Unknown error:", error);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const fetchNotif = async () => {
+      try {
+        const notifRef = query(
+          collection(db, "notifications"),
+          where("receiver_uid", "==", user?.uid),
+          where("is_read", "==", false)
+        );
+        const notifSnapshot = await getDocs(notifRef);
+
+        if (notifSnapshot.docs.length >= 1) {
+          const data: NotificationData[] = notifSnapshot.docs.map((doc) => {
+            const docData = doc.data();
+
+            const timestamp = (docData.timestamp as Timestamp).toDate();
+            const daysDifference = differenceInDays(new Date(), timestamp);
+
+            // If the difference is 7 days or less, show "time ago"
+            const timeAgo = formatDistanceToNow(timestamp, { addSuffix: true });
+
+            // If older than 7 days, show formatted date
+            const formattedDate = format(timestamp, "MMMM dd, yyyy");
+
+            // Decide which one to display
+            const displayDate = daysDifference <= 7 ? timeAgo : formattedDate;
+
+            return {
+              id: doc.id,
+              notif_msg: docData.notif_msg,
+              receiver_uid: docData.receiver_uid,
+              type: docData.type,
+              timestamp: displayDate,
+            };
+          });
+
+          setNotifications(data);
+        } else {
+          setNotifications([]); // Clear notifications if none are found
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error("Error message:", error.message);
+        } else {
+          console.error("Unknown error:", error);
+        }
+      }
+    };
+
+    fetchNotif(); //initial fetch
+
+    const interval = setInterval(() => {
+      fetchNotif();
+    }, 10000); // 10 seconds
+
+    // Clear interval when component unmounts
+    return () => clearInterval(interval);
+  }, [user]);
+
+  const updateIsRead = async (id: string) => {
+    try {
+      const documentRef = doc(db, "notifications", id);
+      await updateDoc(documentRef, { is_read: true });
+
+      return { success: true };
+    } catch (error) {
+      console.error("Error updating status:", error);
+    }
+  };
+
   return (
     <div className="bg-indigo-950 rounded-xl py-3 mb-4 p-4 flex justify-between items-center">
       <div className="flex items-center">
@@ -83,13 +237,62 @@ const HeaderBar = () => {
         <p className="text-sm font-light leading-none">{`${dayOfWeek}, ${formattedDate} ${formattedTime}`}</p>
       </div>
       <div className="flex">
+        <Popover open={popoverOpen} onOpenChange={setPopoverOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="relative"
+              // onClick={handleNotif}
+            >
+              <FaBell className="text-xl" />
+              {notifications.length > 0 && (
+                <Badge
+                  variant="destructive"
+                  className="absolute top-0 right-0 px-1.5 py-0 text-[0.6rem]"
+                >
+                  {notifications.length}
+                </Badge>
+              )}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="mr-[4rem] dark:bg-[#4844B4] bg-[#4844B4] p-2">
+            <ScrollArea className="max-h-60">
+              {loading ? (
+                <Loader2 className="h-10 w-10 animate-spin mx-auto" />
+              ) : notifications.length > 0 ? (
+                <div className="flex flex-col">
+                  {notifications.map((notif) => (
+                    <NotificationCard
+                      key={notif.id}
+                      notificationData={notif}
+                      onClick={() => {
+                        updateIsRead(notif.id);
+                        let tempNotif = notifications;
+                        console.log(tempNotif);
+                        const filteredNotif = tempNotif.filter(
+                          (n) => n.id !== notif.id
+                        );
+                        console.log(filteredNotif);
+                        setNotifications([...filteredNotif]);
+                        setPopoverOpen(false);
+                      }}
+                    />
+                  ))}
+                </div>
+              ) : (
+                <p>There are currently no new notifications</p>
+              )}
+            </ScrollArea>
+          </PopoverContent>
+        </Popover>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="default" size="icon">
               <FaUserCircle className="text-2xl" />
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56 dark:bg-[#4844B4] mr-10">
+          <DropdownMenuContent className="w-56 dark:bg-[#4844B4] bg-[#4844B4] mr-10">
             <DropdownMenuLabel>{user?.email ?? "Loading"}</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
---
+++ src/components/lingkod/notification-card.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import {
+  FaTachometerAlt,
+  FaUser,
+  FaEnvelope,
+  FaFileAlt,
+  FaCalendarAlt,
+  FaBolt,
+  FaHome,
+} from "react-icons/fa";
+import { NotificationData } from "./headerbar";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  notificationData: NotificationData;
+  onClick: () => void;
+};
+
+const NotificationCard = ({ notificationData, onClick }: Props) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    switch (notificationData.type) {
+      case "user":
+        // revalidatecache
+        router.push("/lingkod/residents");
+        break;
+      case "bill":
+        // revalidatecache
+        router.push("/lingkod/bill");
+        break;
+      case "request":
+        // revalidatecache
+        router.push("/lingkod/request");
+        break;
+      case "report":
+        // revalidatecache
+        router.push("/lingkod/reports");
+        break;
+      case "household":
+        // revalidatecache
+        router.push("/lingkod/household");
+        break;
+
+      default:
+        break;
+    }
+    onClick();
+  };
+  return (
+    <div
+      onClick={handleClick}
+      className="flex items-center gap-4 cursor-pointer hover:bg-[#00000057] p-2"
+    >
+      <div className="text-white text-xl">
+        {notificationData.type == "user" ? (
+          <FaUser />
+        ) : notificationData.type == "bill" ? (
+          <FaBolt />
+        ) : notificationData.type == "request" ? (
+          <FaEnvelope />
+        ) : notificationData.type == "report" ? (
+          <FaFileAlt />
+        ) : (
+          <FaHome />
+        )}
+      </div>
+      <div className="flex flex-col gap-1">
+        <h3 className="text-sm line-clamp-2 text-ellipsis">
+          {notificationData.notif_msg}
+        </h3>
+
+        <p className="italic text-gray-400 text-xs">
+          {notificationData.timestamp}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NotificationCard;
